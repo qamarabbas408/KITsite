baa6f6d4d61ec1631cb47869917706f80746f4d0
Qamar Abbas <qmarabbas715@gmail.com>
2023-04-08 14:18:04 +0500

    Added Navigation
    Added Navigation

---
+++ admin/includes/navigation.php
@@ -0,0 +1,48 @@
+<nav class="navbar navbar-expand-lg  ">
+    <div class="container-fluid">
+        <!-- Navbar Brand -->
+        <a class="navbar-brand" href="index.php">
+            <ion-icon size="large" class="book-icon text-warning" name="settings-outline"></ion-icon>
+            <span style="font-size:1.5rem">Admin Panel</span>
+        </a>
+        <button class="navbar-toggler bg-warning" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav  mb-2 mb-lg-0 ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " aria-current="page" href="#">Course</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " aria-current="page" href="#">Student</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " aria-current="page" href="#">Instructor</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a href=""
+                        class="d-none d-lg-none d-md-block text-decoration-none border border-warning rounded-pill text-center">Logout</a>
+                    <button class="user d-lg-block d-md-none  " type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false ">
+                        <div class="d-flex align-items-center  ">
+                            <span class="nav-link d-inline p-0 ">Zafar</span>
+                            <img src="../../images/testi1.jpg" class="img-fluid rounded-circle" alt="">
+                        </div>
+
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="#">Logout</a></li>
+                    </ul>
+
+                </li>
+            </ul>
+
+        </div>
+        
+    </div>
+</nav>
